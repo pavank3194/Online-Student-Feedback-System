062b5845c936709613e984d9ea8ed8e5a83a1687
pavank3194 <108045346+pavank3194@users.noreply.github.com>
2023-02-11 19:06:09 +0530

    Revert "Initial commit"

--- admin/delete_contact.php
+++
@@ -1,8 +0,0 @@
-<?php
-include('../dbconfig.php');
-	
-	$info=$_GET['id'];
-	
-	mysqli_query($conn,"delete from contact where id='$info'");
-	header('location:contactus.php');
-?>
--- admin/delete_faculty.php
+++
@@ -1,8 +0,0 @@
-<?php
-include('../dbconfig.php');
-	
-	$info=$_GET['id'];
-	
-	mysqli_query($conn,"delete from faculty where id='$info'");
-	header('location:manage_faculty.php');
-?>
